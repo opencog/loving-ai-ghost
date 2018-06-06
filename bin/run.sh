51a413e776e0c8a85ce25fe9b7900873be7ca626
DevHEAD <devhead@devhead.com>
2018-06-06 17:20:19 +0800

    update run script to not use ECAN and feedback when rules are loaded

--- bin/run.sh
+++ bin/run.sh
@@ -11,11 +11,15 @@ HR_WORKSPACE="$(hr env | grep HR_WORKSPACE | cut -d = -f 2)"
 # telnet the commands for loading the loving-ai ghost agent
 printf "Loading loving-ai-ghost...\n\n"
 telnet localhost 17001 <<EOF
+scm
+(ghost-af-only #f) (ghost-set-sti-weight 0)
 (ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/concepts-eddie.ghost")
 (ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/introduction-eddie.ghost")
 (ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/awareness_practice_1.ghost")
 (ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/awareness_practice_2.ghost")
 (ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/listening.ghost")
 (ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/closing.ghost")
+(ghost-parse-file "$HR_WORKSPACE/OpenCog/loving-ai-ghost/menu.ghost")
+(cog-execute! (Put (DefinedSchema "say") (List (Concept "System is ready!") (Concept ""))))
 EOF
 printf "\nLoaded\n"
