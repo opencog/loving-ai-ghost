27be47219ca1b364845eb653213ac4844fd5a458
Eddie Monroe <eddiemonroe@gmail.com>
2018-06-07 20:16:08 -0700

    cleanup, formatting, some minor edits

--- lai-scheme-functions.scm
+++ lai-scheme-functions.scm
@@ -5,29 +5,32 @@
 
 ; Add an emotion to the list of detected emotion
 (define-public (register-emotion emo)
- (set! emotions (cons emo emotions))
- (True)
+    (set! emotions (cons emo emotions))
+    (True)
 )
 
 ; Returns dialogue text about detected emotions
 ; within a ConceptNode.
 (define-public (detected-emotions)
- (define emos (map cog-name emotions))
- (define emotions-string
-  (if (not (null? emos))
-   (if (eq? (length emos) 1)
-    (car emos)
-    (string-append (string-join (list-head emos (- (length emotions) 1)) ", ")
-     ", and " (list-ref emos (- (length emos) 1)))
-   )
-   ""
-  )
- )
- (Concept emotions-string)
+    ; Pull the emotion names from list of WordNodes
+    (define emos (map cog-name emotions))
+    (define emotions-string
+        (if (not (null? emos))
+            (if (eq? (length emos) 1)
+                (car emos)
+                (string-append
+                    (string-join (list-head emos (- (length emotions) 1)) ", ")
+                    ", or " (list-ref emos (- (length emos) 1))
+                )
+            )
+            ""
+        )
+    )
+    (Concept emotions-string)
 )
 
 ; Clear the list of detected emotions
 (define-public (clear-detected-emotions)
- (set! emotions '())
- (True)
+    (set! emotions '())
+    (True)
 )
